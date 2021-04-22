4a1f5e77f45d590239d714b2b8e6065795f4c2a4
michaelannawesome <michaelannawesome@gmail.com>
2021-04-21 17:05:25 -0700

    text on screen

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model Batman</Text>
+          <Text style={styles.subtitle}>Starting at $89,995</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62",
   },
 });
